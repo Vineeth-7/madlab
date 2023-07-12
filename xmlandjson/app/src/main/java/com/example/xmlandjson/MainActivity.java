7828f6afb989f36f46b6ee5354e568ac10bf7018
Vineeth <vinervineeth7@gmail.com>
2023-07-12 11:35:27 +0530

    first commit

---
+++ xmlandjson/app/src/main/java/com/example/xmlandjson/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.xmlandjson;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button parseXMLBtn, parseJSONBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        parseXMLBtn = (Button) findViewById(R.id.main_parse_xml_btn_id);
+        parseJSONBtn = (Button) findViewById(R.id.main_parse_json_id);
+        parseXMLBtn.setOnClickListener(this);
+        parseJSONBtn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        if(view.equals(parseXMLBtn)) {
+            Intent intent = new Intent(this,ViewActivity.class);
+            intent.putExtra("dataType", "xml");
+            startActivity(intent);
+        } else if(view.equals(parseJSONBtn)) {
+            Intent intent = new Intent(this,ViewActivity.class);
+            intent.putExtra("dataType", "json");
+            startActivity(intent);
+        }
+    }
+}
